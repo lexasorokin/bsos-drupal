e196b3ab78aae065bfe44afceaac98b3bfecf7dd
Alex Sorokin <sorokin.developer@gmail.com>
2023-07-26 01:18:20 +0200

    Added user migration module

---
+++ web/modules/custom/user_migration/src/Plugin/migrate/source/D8User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\user_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Source plugin for Drupal 8 user migration.
+ *
+ * @MigrateSource(
+ *   id = "d8_user",
+ *   source_module = "user_migration",
+ * )
+ */
+class D8User extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('users', 'u')
+      ->fields('u', ['uid', 'name', 'mail', 'created', 'changed', 'status']);
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return [
+      'uid' => $this->t('User ID'),
+      'name' => $this->t('Username'),
+      'mail' => $this->t('Email'),
+      'created' => $this->t('Created timestamp'),
+      'changed' => $this->t('Changed timestamp'),
+      'status' => $this->t('User status (0 for blocked, 1 for active)'),
+      // Add other user fields you want to migrate
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'uid' => [
+        'type' => 'integer',
+        'alias' => 'u',
+      ],
+    ];
+  }
+
+}
