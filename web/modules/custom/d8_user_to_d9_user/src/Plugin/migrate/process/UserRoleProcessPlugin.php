ab92243735c586cc80bf24d3bb3c1546208978b1
Alex Sorokin <sorokin.developer@gmail.com>
2023-07-26 21:32:12 +0200

    Updated role process

--- web/modules/custom/d8_user_to_d9_user/src/Plugin/migrate/process/UserRoleProcessPlugin.php
+++ web/modules/custom/d8_user_to_d9_user/src/Plugin/migrate/process/UserRoleProcessPlugin.php
@@ -4,6 +4,7 @@ namespace Drupal\d8_user_to_d9_user\Plugin\migrate\process;
 
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
+use Drupal\migrate\MigrateExecutableInterface;
 
 /**
  * Plugin to process user roles.
@@ -17,7 +18,7 @@ class UserRoleProcessPlugin extends ProcessPluginBase {
   /**
    * {@inheritdoc}
    */
-  public function transform($value, array $migrate_executable, Row $row, $destination_property) {
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     // Split the comma-separated role IDs into an array.
     return explode(',', $value);
   }
