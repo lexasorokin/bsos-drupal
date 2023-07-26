2bf8314f8dad3170b5598de5c4beece92e66a25e
Alex Sorokin <sorokin.developer@gmail.com>
2023-07-26 13:13:03 +0200

    Updated d8 user to d9 migrration module

--- web/modules/custom/d8_user_to_d9_user/src/Plugin/migrate/source/D8User.php
+++ web/modules/custom/d8_user_to_d9_user/src/Plugin/migrate/source/D8User.php
@@ -2,11 +2,7 @@
 
 namespace Drupal\d8_user_to_d9_user\Plugin\migrate\source;
 
-use Drupal\Core\Database\Connection;
-use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
-use Drupal\migrate\Row;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Source plugin for Drupal 8 user migration.
@@ -19,71 +15,23 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class D8User extends SqlBase {
 
   /**
-   * The source database connection.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $sourceDatabase;
-
-  /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, $migration_id = 'd8_user') {
-    // Set the default migration ID to the plugin ID if not provided.
-    $migration_id = $migration_id ?: $plugin_id;
-
-    // Make sure that the migration configuration is correctly defined.
-    if (empty($configuration['source']) || empty($configuration['destination'])) {
-      throw new \InvalidArgumentException('Missing source or destination configuration in migration.');
-    }
-
-    // Skip the 'name' field in the process configuration.
-    if (isset($configuration['process']['name'])) {
-      unset($configuration['process']['name']);
-    }
-
-    // Pass the correct migration ID to create the migration instance.
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('database'),
-      $container->get('plugin.manager.migration')->createInstance($migration_id, $configuration)
-    );
-  }
-
-  /**
-   * Constructs a new D8User source plugin.
-   *
-   * @param array $configuration
-   *   The plugin configuration.
-   * @param string $plugin_id
-   *   The plugin ID.
-   * @param mixed $plugin_definition
-   *   The plugin definition.
-   * @param \Drupal\Core\Database\Connection $source_database
-   *   The source database connection.
-   * @param \Drupal\migrate\Plugin\MigrationInterface $migration
-   *   The migration object.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, Connection $source_database, MigrationInterface $migration) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $source_database, $migration);
-    $this->sourceDatabase = $source_database;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function query(): \Drupal\Core\Database\Query\SelectInterface {
+  public function query() {
     $query = $this->select('users_field_data', 'u')
       ->fields('u', ['uid', 'name', 'mail', 'created', 'changed', 'status']);
+
+    // Join the user roles from the "user_roles" table.
+    $query->leftJoin('user__roles', 'ur', 'u.uid = ur.entity_id');
+    $query->addField('ur', 'roles_target_id', 'roles');
+
     return $query;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function fields(): array {
+  public function fields() {
     return [
       'uid' => $this->t('User ID'),
       'name' => $this->t('Username'),
@@ -91,28 +39,23 @@ class D8User extends SqlBase {
       'created' => $this->t('Created timestamp'),
       'changed' => $this->t('Changed timestamp'),
       'status' => $this->t('User status (0 for blocked, 1 for active)'),
+      'roles' => $this->t('User roles (comma-separated)'),
       // Add other user fields you want to migrate
+      //'langcode' => $this->t('Langcode'),
+      //'timezone' => $this->t('Timezone'),
     ];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getIds(): array {
+  public function getIds() {
     return [
       'uid' => [
         'type' => 'integer',
         'alias' => 'u',
       ],
     ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function prepareRow(Row $row): bool {
-    // Add any additional data processing or transformations here if needed.
-    return parent::prepareRow($row);
   }
 
 }
